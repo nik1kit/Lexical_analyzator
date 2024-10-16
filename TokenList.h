ca739504d2c063bc01154d905e8bd293cdb09db7
nik1kit <49343348+nik1kit@users.noreply.github.com>
2024-10-16 18:49:13 +0300

    Update TokenList.h

--- TokenList.h
+++ TokenList.h
@@ -12,17 +12,15 @@ public:
     void addToken(const Token& token);
     int hashFunction(const std::string& lexeme) const;
 
-    // Объявление функции вывода токенов
     void printTokens(std::ofstream& outputFile);
 
 private:
-    static const int HASH_TABLE_SIZE = 100;
+    static const int HASH_TABLE_SIZE = 1000;
     Token* hashTable[HASH_TABLE_SIZE];
 
-    // Новый массив для хранения токенов в порядке их появления
     static const int MAX_TOKENS = 10000; // Максимальное количество токенов
-    Token* tokenSequence[MAX_TOKENS];
+    Token* tokenSequence[MAX_TOKENS]; // Для вывода токенов по порядку
     int tokenCount; // Счетчик количества добавленных токенов
 };
 
-#endif // TOKENLIST_H
+#endif 
